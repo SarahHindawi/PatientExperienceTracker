92336da0b053c6a46a69ecd3dc4b71d63b5a6f38
Sarah Hindawi <43095633+SarahHindawi@users.noreply.github.com>
2021-02-24 00:21:29 -0400

    Retrieving questions and displaying the form

--- resources/views/Survey/create.blade.php
+++ resources/views/Survey/create.blade.php
@@ -1,80 +1,61 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
 <body>
+<div class="container">
+    <form name="surveyForm" method="post" action="/form" enctype="multipart/form-data">
+        @csrf
 
-@section('content')
-    <div class="container">
-        <form name="surveyForm" method="post" action="/form" enctype="multipart/form-data">
-            @csrf
+        <h1>
+            PREM Survey
+        </h1>
 
-            <div class="row">
-                <h1>
-                    PREM Survey
-                </h1>
-            </div>
-            <div class="form-group row">
-                <label for="disease"
-                       class="col-md-4 col-form-label text-md-right">{{ __('Health Condition') }}</label>
+        @foreach ($questions as $q)
+            <label> {{$q["Text"]}}</label> <!--Display the question-->
 
-                <div class="col-md-6">
-                    <input id="disease" type="text"
-                           class="form-control @error('disease') is-invalid @enderror" name="disease"
-                           value="{{ old('disease') }}" required autocomplete="disease" autofocus>
+            @if ( $q["Type"]  == "DropDown")
+                <select name="{{$q["Text"]}}">
 
-                    @error('disease')
-                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                    @enderror
-
+                    <!--iterate over the options-->
+                    @foreach(explode(",",$q['PossibleResponses']) as $option)
+                        <option value="{{$option}}">{{$option}}</option>
+                    @endforeach
                     <br>
-                    <label for="questionResponse"
-                           class="col-md-4 col-form-label text-md-right">{{ __('Medications:') }}</label>
+                </select>
+                <br> <br>
 
-                    <div class="col-md-6">
-                        <input id="response" type="text"
-                               class="form-control @error('response') is-invalid @enderror" name="response"
-                               value="{{ old('response') }}" required autocomplete="name" autofocus>
-                    </div>
+            @elseif ($q["Type"]  == "Checkbox")
 
-                    @error('questionResponse')
-                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                    @enderror
+                <!--TODO fix: checking more than one box returns only the last checked box-->
+                @foreach(explode(",",$q['PossibleResponses']) as $option)
+                    <input type="checkbox" name="{{$q["Text"]}}" value="{{$option}}">
+                    <label>{{$option}}</label>
+                @endforeach
+                <br>
+                <br> <br>
 
+            @elseif ($q["Type"]  == "Radio")
 
-                    <div class="row pt-4">
-                        <button class="btn btn-primary">Submit</button>
-                    </div>
-                </div>
-            </div>
-        </form>
-    </div>
-@endsection
+                @foreach(explode(",",$q['PossibleResponses']) as $option)
+                    <input type="radio" name="{{$q["Text"]}}" value="{{$option}}">
+                    <label>{{$option}}</label>
+                @endforeach
+                <br>
+                <br> <br>
 
-<main class="py-4">
-    @yield('content')
-</main>
+            @elseif ($q["Type"]  == "Text")
+                <input type="text" name="{{$q["Text"]}}"><br>
+                <br>
+                <br> <br>
 
+            @elseif ($q["Type"]  == "TextArea")
+                <textarea name="{{$q["Text"]}}"></textarea>
+                <br> <br>
+            @endif
+
+        @endforeach
+
+        <button>Submit</button>
+    </form>
+</div>
 </body>
 </html>
