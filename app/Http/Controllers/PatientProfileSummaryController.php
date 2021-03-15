d97413f34ef71b13f33815ad4ec8636d3c611765
Sarah Hindawi <43095633+SarahHindawi@users.noreply.github.com>
2021-03-15 15:33:52 -0300

    Some fixes

--- app/Http/Controllers/PatientProfileSummaryController.php
+++ app/Http/Controllers/PatientProfileSummaryController.php
@@ -7,13 +7,14 @@ use App\Models\Patient;
 use Illuminate\Http\Request;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Auth;
+use DB;
 
 
 class PatientProfileSummaryController extends Controller
 {
     public function index()
     {
-        
+
         //Checking if an Admin is not logged in if they are not redirect to adminlogin page.
         if(!Auth::guard('admin')->check()){
 
@@ -22,10 +23,10 @@ class PatientProfileSummaryController extends Controller
                 return redirect('/');
             }
             return redirect('/adminlogin');
-        }     
-        
+        }
+
         $data = Patient::paginate(10);
-        return view('ProfileSummary')->with('data' , $data);
+        return view('ProfileSummary')->with('data', $data);
     }
 
     public function search(Request $request)
@@ -36,20 +37,47 @@ class PatientProfileSummaryController extends Controller
                 //TODO redirect to Patient Dashbaord with unauthorized message.
             }
             return redirect('/adminlogin');
-        }  
+        }
 
-        $data = \DB::table('patient_profile');
-        if ($request->email) {
-            $data = $data->where('email', 'LIKE', "%" . $request->email . "%");
+        $this->validate($request, [
+            'inputEmail' => 'required|email',
+            'inputFirstName' => 'required',
+            'inputLastName' => 'required',
+        ]);
+
+
+        $data = DB::table('PATIENT_PROFILE');
+        if (!empty($request->inputEmail)) {
+            if ($request->inputEmail) {
+                $data = $data->where('Email', 'LIKE', "%" . $request->inputEmail . "%");
+            }
         }
-        if ($request->firstName) {
-            $data = $data->where('firstName', 'LIKE', "%" . $request->firstName . "%");
+
+        if (!empty($request->inputFirstName)) {
+            if ($request->inputFirstName) {
+                $data = $data->where('FirstName', 'LIKE', "%" . $request->inputFirstName . "%");
+            }
         }
-        if ($request->lastName) {
-            $data = $data->where('lastName', 'LIKE', "%" . $request->lastName);
+        if (!empty($request->inputLastName)) {
+            if ($request->inputLastName) {
+                $data = $data->where('LastName', 'LIKE', "%" . $request->inputLastName)->get();
+            }
         }
-        $data = $data->paginate(10);      
-        
-        return view('GenerateReport');
+
+        if (count($data) == 0) {
+            echo '<script type="text/javascript">alert("No records match the specified data.")</script>';
+            return view('ProfileSummary');
+        }
+
+        $data = (array)$data[0];
+
+        $responses = DB::table('Survey_Responses');
+
+        $responses = $responses->where('Email', 'LIKE', "%" . $request->inputEmail . "%")->get();
+
+
+        $responses = (array)$responses->toArray();
+
+        return view('PatientSummaryResult', ["Summary" => $data, "responses" => $responses['responses']]);
     }
 }
--- resources/views/rootadmin_dashboard_page.blade.php
+++ resources/views/rootadmin_dashboard_page.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <title>Root Admin Dashboard</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link href="{{ asset('https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css')}}" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/admin_dashboard_page.css')}}">
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/admin-reset_password.css')}}">
     <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
