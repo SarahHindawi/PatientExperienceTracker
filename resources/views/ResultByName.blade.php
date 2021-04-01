73c4d3bfe7937f84410caae4ffde3d6428c1d89d
Majd Mayaleh <mmayaleh@upei.ca>
2021-03-31 21:47:52 -0300

    update profile summary page to adjust search by name/ and add another to show the matching names

--- resources/views/ResultByName.blade.php
+++ resources/views/ResultByName.blade.php
@@ -1,9 +1,8 @@
-<!-- this page where admins will put info about a patient and a profile summary about him/her-->
+<!-- this page where admins will show the result of matching names of patient when you do a search with their names-->
 <!DOCTYPE html>
 <html>
-<!-- the head has the title of the page and the link for Bootstrap Framework and the link for the css file  -->
 <head>
-    <title>Patient Report Search</title>
+    <!--<title>Patient Report Search</title>!-->
     <link href="{{ asset('https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css')}}" rel="stylesheet"
           integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/cssFile.css')}}">
@@ -13,22 +12,30 @@
             margin-right:auto;
             width:1519px;
         }
+        table {
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width: 80%;
+        }
+
+        td,
+        th {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 4px;
+        }
+
+        tr:nth-child(even) {
+            background-color: #dddddd;
+        }
     </style>
+
 </head>
-<!-- the body has the content of the page  -->
 <div id="wrapper">
 <body>
 
-@if(isset($message))
-    <p class="alert alert-info" style="text-align:center">{{ $message}}</p>
-@else
-    <p style="text-align:center; height:35px; background-color: #f5f5f5"></p>
-
-@endif
-
-<!-- the title in the top  -->
-<div style=" margin-left:10%">
-    <p class="text-center h2">Patient Profile Search</p>
+<div style=" margin-top:5%; margin-left:10%">
+    <p class="text-center h3">We Found These Matching Names</p>
 </div>
 
 <div class="msb" id="msb">
@@ -77,45 +84,45 @@
                 <!--Logout Option-->
                 <li class="nav-item">
                     <p><a class="text-dark nav-link active" aria-current="page"
-                                              href= "{{ url('/logout')}}" ><img src="{{asset('assets/images/key.png')}}" width="25" height="25" class="d-inline-block align-right"> Logout</a></p>
+                          href= "{{ url('/logout')}}" ><img src="{{asset('assets/images/key.png')}}" width="25" height="25" class="d-inline-block align-right"> Logout</a></p>
                 </li>
             </ul>
         </div>
     </nav>
 </div>
+<!--the table that will show you the result names-->
+<div style="margin-left: 10cm;margin-top: 2cm">
+    <table>
+        <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Age</th>
+            <th>Disease</th>
+            <th>Email</th>
+            <th>Summary</th>
 
+        </tr>
+        <tr>
+            <td>Jack</td>
+            <td>Futterkiste</td>
+            <td>56</td>
+            <td>Blood Presure</td>
+            <td>Jack@gmail.com</td>
+            <td><button type="button" class="btn btn-secondary">Summary</button>
+            </td>
+        </tr>
 
-<form method="post" action="/profilereport" enctype="multipart/form-data" style="width: 600px; margin:15%; margin-top:8%">
-    @csrf
-    <!-- Text box for a patient email that admins will look for -->
-        <div class="mb-3 row">
-        <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
-        <div class="col-sm-10">
-            <input type="email" class="form-control shadow-sm" name="inputEmail">
-            <div style="margin-left:130%; margin-top:-7%; width: 150Px">
-                <button style="width: 200px;" type="submit" class="btn btn-success btn-rounded">Search</button>
-            </div>
-        </div>
-    </div>
-        <br>
-        <!-- Text box for a patient first name that admins will look for -->
-        <div class="mb-3 row">
-        <label for="inputFirstName" class="col-sm-2 col-form-label">First Name </label>
-        <div class="col-sm-10">
-            <input type="text" class="form-control shadow-sm" name="inputFirstName">
-            <div style="margin-left:130%; margin-top:-7%; width: 150Px">
-                <button style="width: 200px;" type="submit" class="btn btn-success btn-rounded">Search By Name</button>
-            </div>
-        </div>
-    </div>
-        <!-- Text box for a last first name that admins will look for -->
-        <div class="mb-3 row">
-        <label for="inputLastName" class="col-sm-2 col-form-label ">Last Name</label>
-        <div class="col-sm-10">
-            <input type="text" class="form-control shadow-sm" name="inputLastName">
-        </div>
-    </div>
-</form>
+        <tr>
+            <td>Jack</td>
+            <td>Futterkiste</td>
+            <td>56</td>
+            <td>Blood Presure</td>
+            <td>Jack@gmail.com</td>
+            <td><button type="button" class="btn btn-secondary">Summary</button>
+            </td>
+        </tr>
+    </table>
+</div>
 
 </body>
 </div>
