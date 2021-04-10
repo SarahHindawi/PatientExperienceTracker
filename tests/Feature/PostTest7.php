6738fae9c957a3c87172ecd4866d0544b43e1b88
nmayaleh <43353706+nmayaleh@users.noreply.github.com>
2021-04-10 20:20:23 -0300

    Test7

--- tests/Feature/PostTest7.php
+++ tests/Feature/PostTest7.php
@@ -1,27 +1,24 @@
 <?php
-//
-//namespace Tests\Feature;
-//
-//use Tests\TestCase;
-//
-//class PostTest7 extends TestCase
-//{
-//    /**
-//     * A basic functional test example.
-//     *
-//     * @return void
-//     */
-//    public function test_asserting_a_json_paths_value()
-//    {
-//        $response = $this->json('POST', '/addQuestion', ["SurveyName" => "Nairouz",
-//            "qNumber" => "3",
-//            "qType" => " PROM",
-//            "qText" => "How would you rate your abdominal pain?"
-//            ]);
-//
-//        $response
-//            ->assertStatus(200);
-//       // ->assertJsonPath('team.owner.name', 'Darian');
-//    }
-//}
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class PostTest7 extends TestCase
+{
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function test_asserting_a_json_paths_value()
+    {
+        $response = $this->json('POST', 'addcondition', ["condition" => "NewCondition"
+            ]);
+
+        $response
+            ->assertStatus(200);
+       // ->assertJsonPath('team.owner.name', 'Darian');
+    }
+}
 
